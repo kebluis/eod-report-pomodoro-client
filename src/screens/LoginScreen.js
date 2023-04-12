bb2b155fd561e06bf0622478d297e3401b92b9ed
Michelle Del Rosario <michelled@codev.com>
2023-04-12 08:27:33 +0800

    Init on Google SSO
    .
    
    .

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,151 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import * as Google from "expo-auth-session/providers/google";
+import { useEffect, useState } from "react";
 
 const LoginScreen = () => {
-  return <View>
-    <Text>
-      Login Screen
-    </Text>
-  </View>
+
+  const [userInfo, setUserInfo] = useState();
+  const [auth, setAuth] = useState();
+  const [requireRefresh, setRequireRefresh] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "88224745094-c55h7behfca3mo6405pbglil3qe5kuim.apps.googleusercontent.com",
+    iosClientId: "88224745094-052grg81h7atv0toa2ai2lt3fgd7p98a.apps.googleusercontent.com",
+    expoClientId: ""
+  });
+
+  useEffect(() => {
+    console.log(response);
+    if (response?.type === "success") {
+      setAuth(response.authentication);
+
+      const persistAuth = async () => {
+        await AsyncStorage.setItem("auth", JSON.stringify(response.authentication));
+      };
+      persistAuth();
+    }
+  }, [response]);
+
+  useEffect(() => {
+    const getPersistedAuth = async () => {
+      const jsonValue = await AsyncStorage.getItem("auth");
+      if (jsonValue != null) {
+        const authFromJson = JSON.parse(jsonValue);
+        setAuth(authFromJson);
+        console.log(authFromJson);
+
+        setRequireRefresh(!AuthSession.TokenResponse.isTokenFresh({
+          expiresIn: authFromJson.expiresIn,
+          issuedAt: authFromJson.issuedAt
+        }));
+      }
+    };
+    getPersistedAuth();
+  }, []);
+
+  const getUserData = async () => {
+    let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: { Authorization: `Bearer ${auth.accessToken}` }
+    });
+
+    userInfoResponse.json().then(data => {
+      console.log(data);
+      setUserInfo(data);
+    });
+  };
+
+  const showUserData = () => {
+    if (userInfo) {
+      return (
+        <View style={styles.userInfo}>
+          <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
+          <Text>Welcome {userInfo.name}</Text>
+          <Text>{userInfo.email}</Text>
+        </View>
+      );
+    }
+  };
+
+  const getClientId = () => {
+    if (Platform.OS === "ios") {
+      return "88224745094-052grg81h7atv0toa2ai2lt3fgd7p98a.apps.googleusercontent.com";
+    } else if (Platform.OS === "android") {
+      return "88224745094-c55h7behfca3mo6405pbglil3qe5kuim.apps.googleusercontent.com";
+    } else {
+      console.log("Invalid platform - not handled");
+    }
+  }
+
+  const refreshToken = async () => {
+    const clientId = getClientId();
+    console.log(auth);
+    const tokenResult = await AuthSession.refreshAsync({
+      clientId: clientId,
+      refreshToken: auth.refreshToken
+    }, {
+      tokenEndpoint: "https://www.googleapis.com/oauth2/v4/token"
+    });
+
+    tokenResult.refreshToken = auth.refreshToken;
+
+    setAuth(tokenResult);
+    await AsyncStorage.setItem("auth", JSON.stringify(tokenResult));
+    setRequireRefresh(false);
+  };
+
+  if (requireRefresh) {
+    return (
+      <View style={styles.container}>
+        <Text>Token requires refresh...</Text>
+        <Button title="Refresh Token" onPress={refreshToken} />
+      </View>
+    )
+  }
+
+  const logout = async () => {
+    await AuthSession.revokeAsync({
+      token: auth.accessToken
+    }, {
+      revocationEndpoint: "https://oauth2.googleapis.com/revoke"
+    });
+
+    setAuth(undefined);
+    setUserInfo(undefined);
+    await AsyncStorage.removeItem("auth");
+  };
+
+
+  return (
+    <View style={styles.container}>
+      {showUserData()}
+      <Button 
+        title={auth ? "Get User Data": "Login"} 
+        onPress={auth ? getUserData : () => promptAsync({ useProxy: false, showInRecents: true })}
+      />
+      {auth ? <Button title="Logout" onPress={logout} /> : undefined}
+      <StatusBar style="auto" />
+    </View>
+  );
+
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profilePic: {
+    width: 50,
+    height: 50
+  },
+  userInfo: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default LoginScreen;
