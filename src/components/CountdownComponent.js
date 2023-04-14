0a2a379f55c82ba049ef1bab7d507ca0a00bc595
Kevin Luis <kevluis10@gmail.com>
2023-04-14 20:48:07 +0800

    notification and haptics added

--- src/components/CountdownComponent.js
+++ src/components/CountdownComponent.js
@@ -1,14 +1,33 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
-import globalStyles from "../css/global";
-
 import LottieView from "lottie-react-native";
+import { selectionAsync } from "expo-haptics";
+import { Audio } from "expo-av";
+
+import globalStyles from "../css/global";
+import ConfirmModal from "./modals/ConfirmModal.js";
 
 const CountdownComponent = ({ _minutes = 1, _seconds = 0 }) => {
   const animation = useRef(null);
   const [minutes, setMinutes] = useState(_minutes);
   const [seconds, setSeconds] = useState(_seconds);
   const [start, setStart] = useState(false);
+  const [show, setShow] = useState(false);
+  const [alarmSound, setAlarmSound] = useState(null);
+
+  useEffect(() => {
+    const init = async () => {
+      const { sound } = await Audio.Sound.createAsync(
+        require("../../assets/alarm.wav")
+      );
+      setAlarmSound(sound);
+    };
+
+    init();
+    return () => {
+      if(!!alarmSound) alarmSound.unloadAsync();
+    };
+  }, []);
 
   useEffect(() => {
     const timeout = (ms) => {
@@ -16,7 +35,7 @@ const CountdownComponent = ({ _minutes = 1, _seconds = 0 }) => {
     };
 
     const deductSeconds = async () => {
-      await timeout(1000);
+      await timeout(300);
       setSeconds((prev) => --prev);
     };
 
@@ -28,13 +47,15 @@ const CountdownComponent = ({ _minutes = 1, _seconds = 0 }) => {
       } else {
         setMinutes(_minutes);
         setSeconds(_seconds);
-        setStart(false);
+        alarmSound.replayAsync();
+        setShow(true);
       }
     };
     if (start) seconds === 0 ? deductMinutes() : deductSeconds();
   }, [seconds]);
 
   const startCountdown = () => {
+    selectionAsync();
     setStart(true);
     setTimeout(() => {
       setMinutes((prev) => --prev);
@@ -50,22 +71,51 @@ const CountdownComponent = ({ _minutes = 1, _seconds = 0 }) => {
 
   return (
     <View style={[globalStyles.vPadding1, styles.wrapper]}>
+      <ConfirmModal
+        onNext={() => {
+          alarmSound.pauseAsync();
+          setShow(false);
+          setStart(false);
+        }}
+        onCancel={() => {
+          alarmSound.pauseAsync();
+          setShow(false);
+          setStart(false);
+        }}
+        isVisible={show}
+        nextText="Break"
+        cancelText="Stop"
+      />
       <Text style={[globalStyles.whiteText, styles.counter]}>
         {formattedTime(minutes)}:{formattedTime(seconds)}
       </Text>
 
       {start ? (
-        <View style={styles.animationContainer}>
-          <LottieView
-            autoPlay
-            ref={animation}
-            style={{
-              width: 200,
-              height: 200,
-            }}
-            source={require("../../assets/backToSchool.json")}
-          />
-        </View>
+        show ? (
+          <View style={styles.animationContainer}>
+            <LottieView
+              autoPlay
+              ref={animation}
+              style={{
+                width: 200,
+                height: 200,
+              }}
+              source={require("../../assets/done.json")}
+            />
+          </View>
+        ) : (
+          <View style={styles.animationContainer}>
+            <LottieView
+              autoPlay
+              ref={animation}
+              style={{
+                width: 200,
+                height: 200,
+              }}
+              source={require("../../assets/backToSchool.json")}
+            />
+          </View>
+        )
       ) : (
         <TouchableOpacity onPress={startCountdown} style={styles.start}>
           <Text>START</Text>
@@ -87,6 +137,7 @@ const styles = StyleSheet.create({
   animationContainer: {
     alignItems: "center",
     justifyContent: "center",
+    marginLeft: 20,
   },
   start: {
     width: 100,
--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import globalStyles from '../css/global'
+import globalStyles from "../css/global";
 import CountdownComponent from "../components/CountdownComponent";
 
 const DashboardScreen = () => {
-  return <View style={styles.container}>
-    <View style={styles.wrapper}>
-
-    <CountdownComponent />
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <CountdownComponent />
+      </View>
     </View>
-  </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
-    height: '100%',
-    backgroundColor: '#ba4949',
+    height: "100%",
+    backgroundColor: "#ba4949",
   },
   wrapper: {
     marginHorizontal: 16,
     marginVertical: 48,
-  }
-
-})
+  },
+});
 
 export default DashboardScreen;
