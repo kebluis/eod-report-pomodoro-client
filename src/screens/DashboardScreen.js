3c0dfc4d19cc2264effbe676cc7025b2b31dc2b6
Kevin Luis <kevluis10@gmail.com>
2023-04-22 15:30:56 +0800

    loading implementation ready

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -3,14 +3,19 @@ import { StyleSheet, Text, View } from "react-native";
 import globalStyles from "../css/global";
 import CountdownComponent from "../components/CountdownComponent";
 import TasklistComponent from "../components/TasklistComponent";
+import LoadingComponent from "../components/LoadingComponent";
 
-const DashboardScreen = () => {
+const DashboardScreen = ({isLoading}) => {
   return (
     <View style={styles.container}>
-      <View style={styles.wrapper}>
-        <CountdownComponent />
-        <TasklistComponent />
-      </View>
+      {isLoading ? (
+        <LoadingComponent isMain/>
+      ) : (
+        <View style={styles.wrapper}>
+          <CountdownComponent />
+          <TasklistComponent />
+        </View>
+      )}
     </View>
   );
 };
