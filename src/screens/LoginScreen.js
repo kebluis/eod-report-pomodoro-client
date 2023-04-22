fa8608472ca4a8d422908d84bf3d1d013c939f54
Michelle Del Rosario <michelled@codev.com>
2023-04-22 13:15:17 +0800

    Init on google sso 2.0 approach
    Setup config vars
    
    Update gitignore

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,135 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity, ImageBackground} from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import getEnvVars from "../environment";
 
-const LoginScreen = () => {
-  return <View>
-    <Text>
-      Login Screen
-    </Text>
-  </View>
-};
+WebBrowser.maybeCompleteAuthSession();
 
-const styles = StyleSheet.create({})
+export default function LoginScreen() {
+  const { apiUrl, configIosClientId, configWebClientId } = getEnvVars();
 
-export default LoginScreen;
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: configWebClientId, // Your Expo client ID for Google Sign-In
+    iosClientId: configIosClientId, // Your iOS client ID for Google Sign-In
+  });
+
+  useEffect(() => {
+    handleEffect();
+  }, [response, token]);
+
+  async function handleEffect() {
+    await AsyncStorage.removeItem("@user")
+    const user = await getLocalUser();
+    console.log("user", user);
+    if (!user) {
+      if (response?.type === "success") {
+        // setToken(response.authentication.accessToken);
+        getUserInfo(response.authentication.accessToken);
+      }
+    } else {
+      setUserInfo(user);
+      console.log("loaded locally");
+    }
+  }
+
+  const getLocalUser = async () => {
+    const data = await AsyncStorage.getItem("@user");
+    if (!data) return null;
+    return JSON.parse(data);
+  };
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {!userInfo ? (
+        <TouchableOpacity style={styles.button} onPress={() => {
+            promptAsync();
+          }}
+        >
+        <Image source={require('../../assets/google_logo.png')} style={styles.logo} />
+        <Text style={styles.text}>Sign in with Google</Text>
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.card}>
+          {userInfo?.picture && (
+            <Image source={{ uri: userInfo?.picture }} style={styles.image} />
+          )}
+          <Text style={styles.text}>Email: {userInfo.email}</Text>
+          <Text style={styles.text}>
+            Verified: {userInfo.verified_email ? "yes" : "no"}
+          </Text>
+          <Text style={styles.text}>Name: {userInfo.name}</Text>
+          {/* <Text style={styles.text}>{JSON.stringify(userInfo, null, 2)}</Text> */}
+        </View>
+      )}
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1,
+    elevation: 3,
+  },
+  logo: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  card: {
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 15,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  text: {
+    color: '#757575',
+    fontWeight: '500',
+  },
+});
